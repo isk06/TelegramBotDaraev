dc7811128587237b1aa3994522106acc3c5c77b0
Iskander Daraev <99560093+Iskander-Daraev@users.noreply.github.com>
2023-01-16 23:24:01 +0300

    Update tg_bot_MY.py

--- tg_bot_MY.py
+++ tg_bot_MY.py
@@ -16,19 +16,19 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    start_buttons = ["Последние новости", "Картинка", "Анонсы"]
+    start_buttons = ["Last news", "Picture", "Announcements"]
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
 
-    await message.answer("Приветствую тебя на моем канале! Нажми на одну из кнопок ниже", reply_markup=keyboard)
+    await message.answer("Welcome to my channel! Click on one of the buttons below", reply_markup=keyboard)
 
-# функция получения последних новостей с сайта https://eec.eaeunion.org/news/
-@dp.message_handler(Text(equals="Последние новости"))
+# function for receiving the latest news from the site https://eec.eaeunion.org/news/
+@dp.message_handler(Text(equals="Last news"))
 async def get_all_news(message: types.Message):
-    with open("news_dict.json") as file: # используем наш словарь с новостями из json-файла
+    with open("news_dict.json") as file: # use our dictionary with news from json-file
         news_dict = json.load(file)
 
-    for k, v in news_dict.items(): # передаем новости в телеграм
+    for k, v in news_dict.items(): # send news to Telegram
         news = f"{v['article_date_timestamp']}\n" \
                f"{v['article_title']}\n" \
                f"{v['article_desc']}\n" \
@@ -36,19 +36,19 @@ async def get_all_news(message: types.Message):
 
         await message.answer(news)
 
-# отправка картинки
+# Sending a picture
 
-@dp.message_handler(Text(equals="Картинка"))
+@dp.message_handler(Text(equals="Picture"))
 async def get_photo(message: types.Message):
     await bot.send_photo(chat_id=chat_id, photo='https://telegram.org/img/t_logo.png')
 
 
-# сообщение об анонсах
+# Announcement message
 
-@dp.message_handler(Text(equals="Анонсы"))
+@dp.message_handler(Text(equals="Announcements"))
 async def get_fresh_news(message: types.Message):
     fresh_news = check_news_update()
-    await message.answer("Анонсов пока нет...")
+    await message.answer("No announcements yet...")
 
 if __name__ == '__main__':
     executor.start_polling(dp)
